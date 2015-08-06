0b0195af29cc5df1117c511dda8bd4672fb96b8f
Tom <redsoxfantom@gmail.com>
2015-08-06 18:25:45 -0400

    Created INoiseGenerator

---
+++ NoiseTest/NoiseGenerators/INoiseGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseTest
+{
+    public interface INoiseGenerator
+    {
+        /// <summary>
+        /// Get the noise at a particular location
+        /// </summary>
+        /// <param name="location">The location to sample for noise</param>
+        /// <returns>a value from 0..1</returns>
+        public double getValue(params double[] location);
+    }
+}
