2b147b5c70571996d2a8d30ac7b3a56cac9ada04
Tom <redsoxfantom@gmail.com>
2015-08-06 18:29:14 -0400

    Added combo box to generate noise

--- NoiseTest/Form1.Designer.cs
+++ NoiseTest/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.mDrawingPanel = new System.Windows.Forms.Panel();
+            this.mCbxGeneratorSelector = new System.Windows.Forms.ComboBox();
+            this.mBtnGenerateNoise = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mDrawingPanel
@@ -38,11 +40,30 @@
             this.mDrawingPanel.Size = new System.Drawing.Size(512, 512);
             this.mDrawingPanel.TabIndex = 0;
             // 
+            // mCbxGeneratorSelector
+            // 
+            this.mCbxGeneratorSelector.FormattingEnabled = true;
+            this.mCbxGeneratorSelector.Location = new System.Drawing.Point(33, 519);
+            this.mCbxGeneratorSelector.Name = "mCbxGeneratorSelector";
+            this.mCbxGeneratorSelector.Size = new System.Drawing.Size(170, 21);
+            this.mCbxGeneratorSelector.TabIndex = 1;
+            // 
+            // mBtnGenerateNoise
+            // 
+            this.mBtnGenerateNoise.Location = new System.Drawing.Point(440, 519);
+            this.mBtnGenerateNoise.Name = "mBtnGenerateNoise";
+            this.mBtnGenerateNoise.Size = new System.Drawing.Size(104, 23);
+            this.mBtnGenerateNoise.TabIndex = 2;
+            this.mBtnGenerateNoise.Text = "Generate Noise";
+            this.mBtnGenerateNoise.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 631);
+            this.Controls.Add(this.mBtnGenerateNoise);
+            this.Controls.Add(this.mCbxGeneratorSelector);
             this.Controls.Add(this.mDrawingPanel);
             this.Name = "Form1";
             this.Text = "NoiseTest";
@@ -53,6 +74,8 @@
         #endregion
 
         private System.Windows.Forms.Panel mDrawingPanel;
+        private System.Windows.Forms.ComboBox mCbxGeneratorSelector;
+        private System.Windows.Forms.Button mBtnGenerateNoise;
     }
 }
 
