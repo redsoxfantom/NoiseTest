8cb2989d9a4121997a435d2041e0f4f96aa8307e
Tom <redsoxfantom@gmail.com>
2015-08-06 18:27:25 -0400

    Created a random generator

---
+++ NoiseTest/NoiseGenerators/RandomGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseTest.NoiseGenerators
+{
+    public class RandomGenerator : INoiseGenerator
+    {
+        private Random gen;
+
+        public RandomGenerator()
+        {
+            gen = new Random();
+        }
+
+        public double getValue(params double[] location)
+        {
+            return gen.NextDouble();
+        }
+    }
+}
