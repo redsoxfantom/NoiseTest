74343cdd78b0a7e6bfc07f0b09a3dc9aa7ba041f
Tom <redsoxfantom@gmail.com>
2015-08-06 22:03:58 -0400

    Added SolidGenerator

---
+++ NoiseTest/NoiseGenerators/SolidGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseTest.NoiseGenerators
+{
+    public class SolidGenerator : INoiseGenerator
+    {
+        public double Value { get; set; }
+
+        public double getValue(params double[] location)
+        {
+            return Value;
+        }
+
+        public void Init()
+        {
+
+        }
+    }
+}
