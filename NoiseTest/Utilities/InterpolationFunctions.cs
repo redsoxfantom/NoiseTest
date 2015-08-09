f9b5e30cceafa98278a93f257afc699b2ace6bd7
Tom <redsoxfantom@gmail.com>
2015-08-09 16:17:51 -0400

    Added cubic interpolation

--- NoiseTest/Utilities/InterpolationFunctions.cs
+++ NoiseTest/Utilities/InterpolationFunctions.cs
@@ -8,7 +8,7 @@ namespace NoiseTest.Utilities
 {
     public class InterpolationFunctions
     {
-        public static double Interpolate(Interpolation interpolationToUse, double currentXVal, double startYVal, double endYVal)
+        public static double Interpolate(Interpolation interpolationToUse, double currentXVal, double startYVal, double endYVal, double beforeStartYVal = 0.0, double afterEndYVal = 0.0)
         {
             switch(interpolationToUse)
             {
@@ -16,6 +16,8 @@ namespace NoiseTest.Utilities
                     return LinearlyInterpolate(currentXVal, startYVal, endYVal);
                 case Interpolation.COSINE:
                     return CosineInterpolate(currentXVal, startYVal, endYVal);
+                case Interpolation.CUBIC:
+                    return CubicInterpolate(currentXVal, startYVal, endYVal, beforeStartYVal, afterEndYVal);
                 default:
                     return 0.0;
             }
@@ -30,6 +32,19 @@ namespace NoiseTest.Utilities
         {
             double newCurrentXVal = (1 - Math.Cos(currentXVal * Math.PI)) / 2;
             return (startYVal * (1 - newCurrentXVal) + endYVal * newCurrentXVal);
+        }
+
+        private static double CubicInterpolate(double currentXVal, double startYVal, double endYVal, double beforeStartYVal, double afterEndYVal)
+        {
+            double currentXValSquared = currentXVal * currentXVal;
+            double a0 = afterEndYVal - endYVal - beforeStartYVal + startYVal;
+            double a1 = beforeStartYVal - startYVal - a0;
+            double a2 = endYVal - beforeStartYVal;
+            double a3 = startYVal;
+
+            double calculatedInterp = a0 * currentXVal * currentXValSquared + a1 * currentXValSquared + a2 * currentXVal + a3;
+
+            return calculatedInterp;
         }
     }
 
