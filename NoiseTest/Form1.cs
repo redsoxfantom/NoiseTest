b653d894904de942ab0c6ab05a985fbd216c871b
Tom <redsoxfantom@gmail.com>
2015-08-09 15:34:58 -0400

    Updated Form to report time taken

--- NoiseTest/Form1.cs
+++ NoiseTest/Form1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
@@ -15,10 +16,18 @@ namespace NoiseTest
     public partial class Form1 : Form
     {
         private NoiseGeneratorManager mManager;
+        private Stopwatch mTimer;
+        private double mStopwatchTicksPerMs;
+        private bool mStopwatchIsHiRes;
 
         public Form1()
         {
             InitializeComponent();
+
+            mTimer = new Stopwatch();
+            long stopwatchFreq = Stopwatch.Frequency;
+            mStopwatchTicksPerMs = (double)stopwatchFreq / 1000.0;
+            mStopwatchIsHiRes = Stopwatch.IsHighResolution;
 
             mManager = new NoiseGeneratorManager();
             mManager.Init();
@@ -30,12 +39,20 @@ namespace NoiseTest
 
         private async void mBtnGenerateNoise_Click(object sender, EventArgs e)
         {
+            mInfoLabel.Text = "Working...";
+            mTimer.Start();
             string selectedGenerator = (string)mCbxGeneratorSelector.SelectedItem;
             int width = mDrawingPanel.Size.Width;
             int height = mDrawingPanel.Size.Height;
+            
             Image noiseImage = await Task.Run<Image>(() => { return mManager.GenerateNoiseImage(selectedGenerator, width, height); });
 
             mDrawingPanel.BackgroundImage = noiseImage;
+            mTimer.Stop();
+            long ticksTaken = mTimer.ElapsedTicks;
+            double msTaken = (double)ticksTaken / mStopwatchTicksPerMs;
+            mTimer.Reset();
+            mInfoLabel.Text = string.Format("Done in {0:F5} ms. Stopwatch {1} high resolution.", msTaken, mStopwatchIsHiRes ? "is" : "is not");
         }
 
         private void mCbxGeneratorSelector_SelectedIndexChanged(object sender, EventArgs e)
