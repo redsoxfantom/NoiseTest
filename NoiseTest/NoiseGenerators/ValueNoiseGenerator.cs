f317a2aed10ff8c3dd36fd74781e91c09b5f2778
Tom <redsoxfantom@gmail.com>
2015-08-09 15:02:11 -0400

    Created ValueNoiseGenerator

---
+++ NoiseTest/NoiseGenerators/ValueNoiseGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseTest.NoiseGenerators
+{
+    public class ValueNoiseGenerator : INoiseGenerator
+    {
+
+        public double getValue(params double[] location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
