7e607a18738924fbd5f23155736d9b23859dec53
Tom <redsoxfantom@gmail.com>
2015-08-09 15:13:53 -0400

    Updated ValueNoiseGenerator to use algorithm enum

--- NoiseTest/NoiseGenerators/ValueNoiseGenerator.cs
+++ NoiseTest/NoiseGenerators/ValueNoiseGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NoiseTest.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace NoiseTest.NoiseGenerators
 {
     public class ValueNoiseGenerator : INoiseGenerator
     {
+        public Interpolation InterpolationToUse { get; set; }
 
         public double getValue(params double[] location)
         {
