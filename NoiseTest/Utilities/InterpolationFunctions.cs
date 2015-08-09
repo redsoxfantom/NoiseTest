0a85373ae8c6f33941c7614ede98e0fe4727e459
Tom <redsoxfantom@gmail.com>
2015-08-09 16:01:47 -0400

    Added Linear Interpolation to InterpolationFunctions class

--- NoiseTest/Utilities/InterpolationFunctions.cs
+++ NoiseTest/Utilities/InterpolationFunctions.cs
@@ -8,6 +8,21 @@ namespace NoiseTest.Utilities
 {
     public class InterpolationFunctions
     {
+        public static double Interpolate(Interpolation interpolationToUse, double currentXVal, double startYVal, double endYVal)
+        {
+            switch(interpolationToUse)
+            {
+                case Interpolation.LINEAR:
+                    return LinearlyInterpolate(currentXVal, startYVal, endYVal);
+                default:
+                    return 0.0;
+            }
+        }
+
+        private static double LinearlyInterpolate(double currentXVal, double startYVal, double endYVal)
+        {
+            return (1 - currentXVal) * startYVal + currentXVal * endYVal;
+        }
     }
 
     public enum Interpolation
