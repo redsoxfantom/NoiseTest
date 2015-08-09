5c91db129bb94947012b8cfa2e41ae3fdf0449fa
Tom <redsoxfantom@gmail.com>
2015-08-09 15:14:08 -0400

    Added random generator that caches values

---
+++ NoiseTest/Utilities/RandomNumberGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseTest.Utilities
+{
+    public class RandomNumberGenerator
+    {
+        private Random mGen;
+        private Dictionary<Tuple<int, int>, double> mCachedValues;
+
+        public RandomNumberGenerator(int seed)
+        {
+            mGen = new Random(seed);
+            mCachedValues = new Dictionary<Tuple<int, int>, double>();
+        }
+
+        public RandomNumberGenerator()
+        {
+            mGen = new Random();
+            mCachedValues = new Dictionary<Tuple<int, int>, double>();
+        }
+
+        public double GetValue(int x, int y)
+        {
+            Tuple<int,int> newKey = new Tuple<int,int>(x,y);
+            if(mCachedValues.ContainsKey(newKey))
+            {
+                return mCachedValues[newKey];
+            }
+            else
+            {
+                double valToReturn = mGen.NextDouble();
+                mCachedValues.Add(newKey, valToReturn);
+                return valToReturn;
+            }
+        }
+    }
+}
