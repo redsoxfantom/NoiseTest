793c343d2d4e14ff5bae84df0f52bcdfca2580bb
Tom <redsoxfantom@gmail.com>
2015-08-09 16:34:40 -0400

    Work on value noise generator

--- NoiseTest/NoiseGenerators/ValueNoiseGenerator.cs
+++ NoiseTest/NoiseGenerators/ValueNoiseGenerator.cs
@@ -10,15 +10,35 @@ namespace NoiseTest.NoiseGenerators
     public class ValueNoiseGenerator : INoiseGenerator
     {
         public Interpolation InterpolationToUse { get; set; }
+        public int Seed { get; set; }
+        public double Frequency { get; set; }
+        public int Octaves { get; set; }
+        public double Persistance { get; set; }
+
+        private RandomNumberGenerator mGen;
+        private double amplitude;
+        private double mFrequency;
 
         public double getValue(params double[] location)
         {
-            throw new NotImplementedException();
+            double total = 0.0;
+            double x = location[0], y = location[1];
+
+            for (int i = 0; i < Octaves; i++)
+            {
+                total += mGen.GetValue(x * mFrequency, y * mFrequency) * amplitude;
+                mFrequency *= 2;
+                amplitude *= Persistance;
+            }
+
+            return total > 1.0 ? 1.0 : total;
         }
 
         public void Init()
         {
-            throw new NotImplementedException();
+            mGen = new RandomNumberGenerator(Seed);
+            amplitude = Persistance;
+            mFrequency = Frequency;
         }
     }
 }
