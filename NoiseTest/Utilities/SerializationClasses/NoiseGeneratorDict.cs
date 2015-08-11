8ca45b8671ce30a510396dde091ff48b9c6fbef4
Tom <redsoxfantom@gmail.com>
2015-08-10 20:30:39 -0400

    Created class to store serialized dictionary of noise generators

---
+++ NoiseTest/Utilities/SerializationClasses/NoiseGeneratorDict.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseTest.Utilities.SerializationClasses
+{
+    [CollectionDataContract(Name="NoiseGenerators",KeyName="NoiseGeneratorName",ValueName="NoiseGenerator")]
+    public class NoiseGeneratorDict : Dictionary<string,INoiseGenerator>
+    {
+    }
+}
